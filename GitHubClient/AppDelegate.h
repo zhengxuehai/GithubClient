328b2817bb693e029fea2adff791dd7f2f563a69
zangqilong <zangqilong@gmail.com>
2016-04-30 23:04:53 +0800

    Initial Commit

---
+++ GitHubClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubClient
+//
+//  Created by 臧其龙 on 16/4/30.
+//  Copyright © 2016年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitHubClient
+//
+//  Created by 臧其龙 on 16/4/30.
+//  Copyright © 2016年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
